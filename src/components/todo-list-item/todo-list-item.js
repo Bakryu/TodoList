c5bdc38ec6852f3760f4fe228d626a1fac218287
Stanislav Bakryu <stasbakryu@gmail.com>
2020-10-31 19:25:51 +0300

    selete button func

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,29 +3,45 @@ import React, { Component } from "react";
 import "./todo-list-item.css";
 
 export default class TodoListItem extends Component {
-  onLabelClick = () => console.log(`click:${this.props.label}`);
+  state = {
+    done: false,
+    important: false,
+  };
+
+  onLabelClick = () =>
+    this.setState((state) => {
+      return {
+        done: !state.done,
+      };
+    });
+  onMarkImportant = () =>
+    this.setState((state) => {
+      return {
+        important: !state.important,
+      };
+    });
 
   render() {
-    const { label, important = false } = this.props;
+    const { label, onDeleted } = this.props;
+    const { done, important } = this.state;
 
-    const style = {
-      color: important ? "steelblue" : "black",
-      fontWeight: important ? "bold" : "normal",
-    };
-
+    let classNames = "todo-list-item-label";
+    if (done) {
+      classNames += " done";
+    }
+    if (important) {
+      classNames += " important";
+    }
     return (
       <span className="todo-list-item">
-        <span
-          className="todo-list-item-label"
-          style={style}
-          onClick={this.onLabelClick}
-        >
+        <span className={classNames} onClick={this.onLabelClick}>
           {label}
         </span>
 
         <button
           type="button"
           className="btn btn-outline-success btn-sm float-right"
+          onClick={this.onMarkImportant}
         >
           <i className="fa fa-exclamation" />
         </button>
@@ -33,6 +49,7 @@ export default class TodoListItem extends Component {
         <button
           type="button"
           className="btn btn-outline-danger btn-sm float-right"
+          onClick={onDeleted}
         >
           <i className="fa fa-trash-o" />
         </button>
